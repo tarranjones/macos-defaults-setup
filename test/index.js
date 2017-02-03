a1c20154380b30594196f99ee40a4035101c77a0
Tarran <tarran@tarrans-imac.home>
2017-02-03 15:33:18 +0000

    removed unused vars

--- test/index.js
+++ test/index.js
@@ -1,12 +1,8 @@
 import assert from 'assert';
-import macosDefaultsSetup from '../lib';
 
 describe('macos-defaults-setup', function () {
 
   it('need some tests ', function () {
-
-
-      // macosDefaultsSetup('../test-files/chrome.json');
     assert(true, 'didn\'t run test, don\'t want to run exec' );
   });
 });
